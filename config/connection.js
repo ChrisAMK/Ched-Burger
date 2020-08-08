695a74980bdc781966027ac04d1ae7c9ee8e8900
Chris <chriskl@live.com>
2020-08-08 22:31:18 +0800

    adding database connection

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,18 @@
 const mysql = require("mysql");
 // Defining the database connection details
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+}
+
 // Connecting to the database with a connect funtion
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 // Exporting the connection variable to be used elsewhere
 module.exports = connection;
